2a0461eb688a68be9f0a1e86448f24e15c720516
Shantanu Vora <shantu.git@gmail.com>
2022-06-10 12:24:52 +0530

    Rename SubmitServlet.java to ContactUsServlet.java and add doGet method in ContactUsServlet.java

--- src/main/java/com/shantanu/ContactUsServlet.java
+++ src/main/java/com/shantanu/ContactUsServlet.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/submit")
-public class SubmitServlet extends HttpServlet {
 
+@WebServlet("/contactus")
+public class ContactUsServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("contactus.jsp");
+		requestDispatcher.forward(request, response);
+	}
+	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		PrintWriter out = response.getWriter();
