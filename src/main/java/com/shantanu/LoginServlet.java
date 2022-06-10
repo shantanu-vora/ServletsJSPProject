6894828394a2f759dfbd6041f8234c299036ce65
Shantanu Vora <shantu.git@gmail.com>
2022-06-10 12:31:08 +0530

    Add LoginServlet.java Controller and define doGet method to load login.jsp page

---
+++ src/main/java/com/shantanu/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.shantanu;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
+		requestDispatcher.forward(request, response);
+		
+//		response.sendRedirect("login.jsp");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+	
+	
+
+}
