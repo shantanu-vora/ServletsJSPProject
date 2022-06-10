db8dab11b62634f748f6bba035d5312dc407b9d1
Shantanu Vora <shantu.git@gmail.com>
2022-06-11 04:40:52 +0530

    Add doGet and doPost methods to DashboardServlet.java

--- src/main/java/com/shantanu/DashboardServlet.java
+++ src/main/java/com/shantanu/DashboardServlet.java
@@ -18,13 +18,37 @@ public class DashboardServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		RequestDao requestDao = new RequestDao();
+		
+		List<Request> list = null;
+		try {
+			list = requestDao.fetchRequest();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("requestList", list);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("dashboard.jsp");
+		requestDispatcher.forward(request, response);
 		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		int requestId = Integer.parseInt(request.getParameter("reqId"));
 		
+		Request req = new Request(requestId);
+		RequestDao requestDao = new RequestDao();
+		
+		try {
+			requestDao.updateRequestStatus(req);
+			doGet(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 
